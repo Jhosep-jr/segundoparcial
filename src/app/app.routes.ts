fba4aef09b8f16eedb53b44bfdb08d102bef9ff8
jhosep-jr <tu-email-asociado@jhosep-jr.com>
2025-09-23 18:37:07 -0400

    feat(home): integrate template layout into Home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { Layout } from './layout/layout';
+import { Home } from './home/home';
+import { About } from './about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+{
+    path:'',
+    component:Layout,
+    children:[
+        {
+          path:'',
+          component:Home,
+          title:'inicio'
+        },
+        {
+          path:'about',
+          component:About,
+          title:'Un dia con nosotros'
+        }
+    ]
+}
+
+
+];
